fce2d52f4a8b898b42609fd1ecf5800373f272f7
unknown <jtjackson325@gmail.com>
2021-08-31 17:58:15 -0600

    got the code mostly modulazied

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundlleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    }, 
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + "/dist",
+        filename: "[name].bundle.js"
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundlleAnalyzerPlugin({
+            analyzerMode: "static", // the report ouputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 };
